863c4c09f1f94f421c56bac64727da2606b31c23
Yessenia Aguilar <yaguilar@wlapcs.org>
2018-01-09 19:59:10 +0000

    build keyboard inputs

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
 function setup(){
     createCanvas(600,400);
 }
-
 function draw(){
-    
+    if(keyIsPressed){
+       blendMode(LIGHTEST)
+        if(key=="r"){
+            background(255,0,0);
+        }
+        if(key=="g"){
+            background(0,255,0);
+        }
+        
+        if(key=="b"){
+            background(0,0,255);
+        }
+        if(keyCode==32){
+            background(255,255,255,); 
+        }
+    }
+    else{
+        background(50);
+    }
+}
+
+function keyReleased(){
+    blendMode(NORMAL);
 }
